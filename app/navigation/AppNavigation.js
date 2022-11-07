7dc7056c0390bcb0ad133b3c9cb6896f2ac4ef13
Dhruv Harsora <harsoradhruv@gmail.com>
2022-11-07 17:50:03 +0530

    feat: Implement dark-light theme using navigation

--- app/navigation/AppNavigation.js
+++ app/navigation/AppNavigation.js
@@ -1,23 +1,21 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  NavigationContainer,
-} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import { useColorScheme } from 'react-native';
 import { ROUTES } from '../constant';
 import { HomeScreen } from '../modules';
+import { Colors } from '../theme';
 
 const RootStack = createStackNavigator();
 
 const AppContainer = () => {
-  const { scheme } = useColorScheme();
+  const theme = useColorScheme();
 
   return (
     <>
-      <NavigationContainer theme={scheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <RootStack.Navigator>
+      <NavigationContainer
+        theme={theme === 'dark' ? Colors.dark : Colors.light}>
+        <RootStack.Navigator screenOptions={{ headerShown: false }}>
           <RootStack.Screen name={ROUTES.Home} component={HomeScreen} />
         </RootStack.Navigator>
       </NavigationContainer>
